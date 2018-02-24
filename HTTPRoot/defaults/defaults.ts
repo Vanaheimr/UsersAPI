bc23b203e8eaed9549bbb39564097d2e4e46bba7
Achim Friedland <github@ahzf.de>
2018-02-24 19:16:45 +0100

    CSS updates

--- HTTPRoot/defaults/defaults.ts
+++ HTTPRoot/defaults/defaults.ts
@@ -162,3 +162,124 @@ function UpdateI18NDescription(DescriptionDiv: HTMLDivElement,
     }
 
 }
+
+
+
+
+
+interface DivElementAction {
+    (DoWith: HTMLDivElement): void;
+}
+
+class View {
+
+    myself:           HTMLDivElement;
+    HeadlineDiv:      HTMLDivElement;
+    ContentDiv:       HTMLDivElement;
+    CurrentGroupDiv:  HTMLDivElement;
+    ButtonDiv:        HTMLDivElement;
+
+    constructor(Parent:        HTMLDivElement,
+                Prefix:        string,
+                HeadlineText:  string) {
+
+        this.myself = Parent.appendChild(document.createElement('div')) as HTMLDivElement;
+        this.myself.id = Prefix + "View";
+
+        this.HeadlineDiv = this.myself.appendChild(document.createElement('div')) as HTMLDivElement;
+        this.HeadlineDiv.id        = "headline";
+        this.HeadlineDiv.innerText = HeadlineText;
+
+        this.ContentDiv  = this.myself.appendChild(document.createElement('div')) as HTMLDivElement;
+        this.ContentDiv.id         = "content";
+
+        this.ButtonDiv   = this.myself.appendChild(document.createElement('div')) as HTMLDivElement;
+        this.ButtonDiv.id          = "buttons";
+
+        this.CurrentGroupDiv = this.ContentDiv;
+
+    }
+
+    CreateGroup(Prefix:  string,
+                Visible: boolean = true) {
+
+        var GroupDiv = this.ContentDiv.appendChild(document.createElement('div')) as HTMLDivElement;
+        GroupDiv.id = Prefix + "Group";
+        GroupDiv.className = "rowGroup";
+
+        if (!Visible)
+            GroupDiv.style.display = "none";
+
+        this.CurrentGroupDiv = GroupDiv;
+        return GroupDiv;
+
+    }
+
+    CreateRow(Prefix: string,
+        KeyText: string,
+        greeter: DivElementAction = null) {
+
+        return this.CreateRow2(
+            Prefix,
+            KeyText,
+            false,
+            greeter);
+
+    };
+
+    CreateRow2(
+        Prefix: string,
+        KeyText: string,
+        keyTop: boolean = false,
+        greeter: DivElementAction = null) {
+
+
+        var DivRow = this.CurrentGroupDiv.appendChild(document.createElement('div')) as HTMLDivElement;
+        DivRow.id = Prefix + "Row";
+        DivRow.className = "row";
+
+        var DivKey = DivRow.appendChild(document.createElement('div')) as HTMLDivElement;
+        DivKey.id = Prefix + "Key";
+        DivKey.className = "key" + (keyTop ? " keyTop" : "");
+        DivKey.innerText = KeyText;
+
+        var DivValue = DivRow.appendChild(document.createElement('div')) as HTMLDivElement;
+        DivValue.id = Prefix + "Value";
+        DivValue.className = "value";
+
+        if (greeter != null)
+            greeter(DivValue);
+
+        return DivValue;
+
+    }
+
+    CreateButton(Prefix: string,
+                 Name:   string) {
+
+        var ButtonDiv = this.ButtonDiv.appendChild(document.createElement('button')) as HTMLButtonElement;
+        ButtonDiv.id = Prefix + "Button";
+        ButtonDiv.className = "button";
+        ButtonDiv.innerHTML = Name;
+
+    }
+
+    CancelButton(Prefix: string,
+                 Name:   string) {
+
+        var aa = this;
+
+        var ButtonDiv = this.ButtonDiv.appendChild(document.createElement('button')) as HTMLButtonElement;
+        ButtonDiv.id = Prefix + "Button";
+        ButtonDiv.className = "button";
+        ButtonDiv.innerText = Name;
+        ButtonDiv.onclick = function (this: HTMLElement, ev: MouseEvent) {
+
+            aa.myself.remove();
+
+        };
+
+    }
+
+
+}
