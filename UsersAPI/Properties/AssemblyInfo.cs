35848acb7e176e82650ceb80b07eec9ae77c1cde
Achim Friedland <github@ahzf.de>
2020-01-09 23:35:42 +0100

    Happy new year!

--- UsersAPI/Properties/AssemblyInfo.cs
+++ UsersAPI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GraphDefined GmbH")]
 [assembly: AssemblyProduct("UsersAPI")]
-[assembly: AssemblyCopyright("Copyright © 2015-2019 GraphDefined GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2015-2020 GraphDefined GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
