5eb1d225438532f1c9b33f8c7466c5cdf6990b72
Achim Friedland <achim.friedland@graphdefined.com>
2021-08-15 13:22:19 +0200

    API key refactorings

---
+++ UsersAPI/Entities/APIKeys/Results/AddAPIKeyResult.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Copyright (c) 2014-2021, Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * This file is part of OpenDataAPI <http://www.github.com/GraphDefined/OpenDataAPI>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+using org.GraphDefined.Vanaheimr.Illias;
+
+#endregion
+
+namespace social.OpenData.UsersAPI
+{
+
+    public class AddAPIKeyResult : AResult<APIKey>
+    {
+
+        public APIKey APIKey
+            => Object;
+
+        public Organization  Organization   { get; internal set; }
+
+
+        public AddAPIKeyResult(APIKey        APIKey,
+                               EventTracking_Id  EventTrackingId,
+                               Boolean           IsSuccess,
+                               String            Argument           = null,
+                               I18NString        ErrorDescription   = null,
+                               Organization      Organization       = null)
+
+            : base(APIKey,
+                   EventTrackingId,
+                   IsSuccess,
+                   Argument,
+                   ErrorDescription)
+
+        {
+
+            this.Organization = Organization;
+
+        }
+
+
+        public static AddAPIKeyResult Success(APIKey        APIKey,
+                                              EventTracking_Id  EventTrackingId,
+                                              Organization      Organization = null)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   true,
+                                   null,
+                                   null,
+                                   Organization);
+
+
+        public static AddAPIKeyResult ArgumentError(APIKey        APIKey,
+                                                    EventTracking_Id  EventTrackingId,
+                                                    String            Argument,
+                                                    String            Description)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   false,
+                                   Argument,
+                                   I18NString.Create(Languages.en,
+                                                     Description));
+
+        public static AddAPIKeyResult ArgumentError(APIKey        APIKey,
+                                                    EventTracking_Id  EventTrackingId,
+                                                    String            Argument,
+                                                    I18NString        Description)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   false,
+                                   Argument,
+                                   Description);
+
+
+        public static AddAPIKeyResult Failed(APIKey        APIKey,
+                                             EventTracking_Id  EventTrackingId,
+                                             String            Description,
+                                             Organization      Organization  = null)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   false,
+                                   null,
+                                   I18NString.Create(Languages.en,
+                                                     Description),
+                                   Organization);
+
+        public static AddAPIKeyResult Failed(APIKey        APIKey,
+                                             EventTracking_Id  EventTrackingId,
+                                             I18NString        Description,
+                                             Organization      Organization  = null)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   false,
+                                   null,
+                                   Description,
+                                   Organization);
+
+        public static AddAPIKeyResult Failed(APIKey        APIKey,
+                                             EventTracking_Id  EventTrackingId,
+                                             Exception         Exception,
+                                             Organization      Organization  = null)
+
+            => new AddAPIKeyResult(APIKey,
+                                   EventTrackingId,
+                                   false,
+                                   null,
+                                   I18NString.Create(Languages.en,
+                                                     Exception.Message),
+                                   Organization);
+
+    }
+
+}
